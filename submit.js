4aecd05d54bdd69aa439e9a58b6e5f06395af3ad
malmi <mhd1ikhmal@gmail.com>
2026-01-19 15:57:45 +0800

    added some changes

--- submit.js
+++ submit.js
@@ -1,5 +1,12 @@
 
 
-function onSubmit(){
+function nextPage(){
+    let checkID = document.getElementById("studentID").value;
+    let checkPass = document.getElementById("pass").value;
+    
+    
+    if(checkID && checkPass){
+        window.location.href = "studentReg.html";
+    }
     
 }
