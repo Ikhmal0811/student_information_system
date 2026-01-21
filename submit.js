f81eecb55be76cf5adc09bde1d0e6de7d251409c
malmi <mhd1ikhmal@gmail.com>
2026-01-21 10:41:04 +0800

    added icon on nav

--- submit.js
+++ submit.js
@@ -5,8 +5,13 @@ function nextPage(){
     let checkPass = document.getElementById("pass").value;
     
     
-    if(checkID){
-        window.location.href = "studentReg.html";
+    if(checkID == "test"){
+        if(checkPass == "test1"){
+            window.location.href = "studentReg.html";
+        }else{
+            
+        }
+        
     }
     
 }
